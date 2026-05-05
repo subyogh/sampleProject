90ea4e0842f3267722cadb283e3f39604cc76f5b
Shubham Yadav <shubham.yadav262@gmail.com>
2026-05-05 13:21:08 +0530

    add

--- routes/user_router.js
+++ routes/user_router.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controller/user_controller');
 
 router.post('/register', userController.register);
-router.get('/users', userController.getAll);
-router.get('/users/:id', userController.getOne);
-router.put('/users/:id', userController.update);
+// router.get('/users', userController.getAll);
+// router.get('/users/:id', userController.getOne);
+// router.put('/users/:id', userController.update);
 
 
 router.delete('/users/:id', userController.delete);
